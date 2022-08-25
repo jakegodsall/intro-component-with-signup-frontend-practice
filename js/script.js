a3b5cd976cc6d3bb64c50facd4035ddc36653f40
jakegodsall <jake.edward.godsall@gmail.com>
2022-08-25 10:20:42 +0100

    added error messages for empty inputs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
-const btn = document.querySelector('.submit').addEventListener('click', function (e) {
+// elements
+const form = document.querySelector('form');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email-address');
+const password = document.getElementById('password');
+
+function isEmpty() {
+
+}
+
+console.log(form);
+
+form.addEventListener('submit', function (e) {
+
+    const elements = Array.from(form.elements);
+
+
+    console.log(elements);
+
+    elements.forEach(function (element) {
+        if (element.value == '' && element.type !== 'submit') {
+            emptyInput(element);
+        } else {
+
+        }
+    });
+
     e.preventDefault();
-})
+});
+
+function emptyInput(inputElement) {
+    const formRow = inputElement.parentNode;
+
+    if (formRow.children.length == 1) {
+        const p = document.createElement('p');
+        p.classList.add('error');
+        p.innerText = `${inputElement.placeholder} cannot be empty`;
+        formRow.appendChild(p);
+    }
+
+}
